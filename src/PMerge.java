b24adc37a2f27a629074fcdb786ab394ff56f141
kyc375 <ckravis@utexas.edu>
2018-02-01 21:04:15 -0600

    PMerge.java

--- src/PMerge.java
+++ src/PMerge.java
@@ -28,15 +28,36 @@ public class PMerge{
   private class PMergeTask implements Callable<Integer>{
     private int element;
     private int[] a;
+    private int index;
 
-    public PMergeTask(int element, int[] a){
+    public PMergeTask(int element, int[] a, int index){
       this.element = element;
       this.a = a;
+      this.index = index;
+    }
+    public int binarysearch(int[] a, int left, int right, int element) {
+    	if(right >= 1) {
+    		int mid = left + (right - 1) / 2;
+    		if(a[mid] == element) {
+    			return mid;
+    		}else if(a[mid] > element) {
+    			return binarysearch(a, left, mid-1, element);
+    		}else if(a[mid] < element) {
+    			return binarysearch(a, mid+1, right, element);
+    		}
+    	}
+    	return -1;
     }
 
     @Override
     public Integer call() throws Exception {
-      return 0;
+    	int index2 = binarysearch(a, 0, a.length, element);
+    	if(index2 != -1) {
+    		index += index2;
+    	}else{
+    		return -1;      //error in case the array size is less than 1
+    	}
+    	return index;
     }
   }
 }
